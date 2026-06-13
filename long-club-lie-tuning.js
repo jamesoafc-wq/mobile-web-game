3a3c92ee895e759f72fe6ebc4427d05ff38476a4
jamesoafc-wq <jamesoafc@hotmail.com>
2026-06-13 21:27:57 +0100

    Tune long clubs off the deck

---
+++ long-club-lie-tuning.js
@@ -0,0 +1,20 @@
+// Long-club lie tuning.
+// Driver and 3W are mainly tee-shot clubs in this prototype.
+// Off the deck they should be very hard and should not produce big distance.
+
+clubs.driver.carry.fairway = 45;
+clubs.driver.carry.rough = 15;
+clubs.driver.carry.green = 8;
+clubs.driver.carry.sand = 0;
+clubs.driver.accuracy = 12.5;
+
+clubs.wood3.carry.fairway = 75;
+clubs.wood3.carry.rough = 25;
+clubs.wood3.carry.green = 10;
+clubs.wood3.carry.sand = 0;
+clubs.wood3.accuracy = 10.5;
+
+// skillClubDifficulty is created by skill-shot.js. These values make long clubs
+// especially unforgiving while still leaving driver/3W tee shots playable.
+skillClubDifficulty.driver = 0.92;
+skillClubDifficulty.wood3 = 0.82;
